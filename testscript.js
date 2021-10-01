708425329a21176c1c9f7a394f60945ffec2a79d
Michael Gubb <michaelgubb@developersinstitute.co.nz>
2021-10-01 14:03:06 +1300

    Create initial html and JS

---
+++ testscript.js
@@ -0,0 +1,35 @@
+const testcode = (code1, useDebugger) => {
+  if (useDebugger) {
+    debugger;
+  }
+
+
+  console.log("Start");
+  //console.log(a);
+  if (code1) {
+    let a;
+    console.log(a);
+    //var a;
+    //let a;
+    a = 5;
+    console.log(a);
+  }
+  //console.log(a);
+  //var a;
+
+  let myAnimal = "dog";
+  if (myAnimal == "Dog") {
+    console.log("My animal is Dog");
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    const a = "5";
+  }
+  //
+  console.log("Finish");
+};
